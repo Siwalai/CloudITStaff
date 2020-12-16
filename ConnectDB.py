641b8f0b15962da0396773e0e1ee7d84c8ddbc9b
Siwalai <siwalai.s@fitm.kmutnb.ac.th>
2020-12-16 14:16:26 +0700

    test2

---
+++ ConnectDB.py
@@ -0,0 +1,25 @@
+import psycopg2
+try:
+    connection = psycopg2.connect(user="webadmin",
+                                  password="EQPilg14001",
+                                  host="node7537-it-cloud44.app.ruk-com.cloud:11063",
+                                  port="11063",
+                                  database="CloudDB")
+
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print(connection.get_dsn_parameters(), "\n")
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    # closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
